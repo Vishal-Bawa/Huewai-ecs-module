5c3d61a8faaea4d50b5d8cab708f6febab6ed0fa
Vishal Puri <vishalbawa7082@gmail.com>
2025-05-14 21:07:03 +0530

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -4,14 +4,6 @@ resource "tls_private_key" "example" {
 }
 
 resource "huaweicloud_compute_keypair" "keypair" {
-  name       = "${var.instance_name}-key"
+  name       = var.keypair_name
   public_key = tls_private_key.example.public_key_openssh
 }
-
-resource "local_file" "private_key" {
-  content         = tls_private_key.example.private_key_pem
-  filename        = "${path.module}/${var.instance_name}-key.pem"
-  file_permission = "0400"
-}
-
-
