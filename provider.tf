b7ca133d5186315610fe17685f2bfaab33cce94f
Vishal Puri <vishalbawa7082@gmail.com>
2025-05-14 18:25:09 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,7 +13,7 @@ terraform {
 
 provider "huaweicloud" {
   region     = "la-north-2"
-  access_key = "HPUALBP1METRKR5UWHY5"
-  secret_key = "iNdjG6SiTqzaVS8mV2XWlj30ay0IjjWtn0c2Fdb9"
+  access_key = 
+  secret_key = 
   cloud      = "myhuaweicloud.com"
 }
