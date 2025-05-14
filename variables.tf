fe6fb714fdb6473e93433ee79d0673f7bcbf3500
Vishal Puri <vishalbawa7082@gmail.com>
2025-05-14 15:58:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "image_id" { type = string }
 variable "vpc_network_uuid" { type = string }
 #variable "keypair_name" { type = string }
 variable "secgroup_name" { type = string }
-variable "ecs_eip_address" { type = string }
+#variable "ecs_eip_address" { type = string }
 variable "private_key_content" {
   type      = string
   sensitive = true
