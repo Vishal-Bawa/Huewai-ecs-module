219b5e217ea2d128f5a41e6fa737bc2d90269a1b
Vishal Puri <vishalbawa7082@gmail.com>
2025-05-14 21:34:53 +0530

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -3,7 +3,7 @@ resource "tls_private_key" "example" {
   rsa_bits  = 4096
 }
 
-resource "huaweicloud_compute_keypair" "keypair" {
-  name       = var.keypair_name
+resource "huaweicloud_compute_keypair" "ecs_key" {
+  name       = var.keypair_name != "" ? var.keypair_name : "ecs-key"
   public_key = tls_private_key.example.public_key_openssh
 }
