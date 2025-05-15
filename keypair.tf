0a8c34da721d976b6323d28f3a4de36ff7cc6233
Vishal Puri <vishalbawa7082@gmail.com>
2025-05-15 11:15:41 +0530

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -11,3 +11,19 @@ resource "huaweicloud_compute_keypair" "ecs_key" {
   name       = var.keypair_name != "" ? var.keypair_name : "ecs-key-${random_id.suffix.hex}"
   public_key = tls_private_key.example.public_key_openssh
 }
+
+# Save private key to root directory
+resource "local_file" "pem" {
+  content  = tls_private_key.example.private_key_pem
+  filename = "${path.root}/terraform-key.pem"
+}
+
+# Set permissions on the key file
+resource "null_resource" "chmod_pem" {
+  depends_on = [local_file.pem]
+
+  provisioner "local-exec" {
+    command = "chmod 400 ${path.root}/terraform-key.pem"
+  }
+}
+
